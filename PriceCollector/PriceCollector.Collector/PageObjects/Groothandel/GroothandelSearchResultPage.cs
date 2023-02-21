47a17d065b4dbc7d9b00a3459326c6e19f98b13c
Oleksandr Zhurba <Oleksandr.Zhurba@bigchange.com>
2023-02-21 17:49:09 +0200

    fixed Groothandel

--- PriceCollector/PriceCollector.Collector/PageObjects/Groothandel/GroothandelSearchResultPage.cs
+++ PriceCollector/PriceCollector.Collector/PageObjects/Groothandel/GroothandelSearchResultPage.cs
@@ -11,7 +11,7 @@ namespace PriceCollector.Collector.PageObjects.Groothandel
 
         public IWebElement ProductItemBlock(string itemId) => driver.UsingDriverImplicitTimeout(() => ProductHrefElement(itemId).FindElement(By.XPath("./ancestor::li")), TimeSpan.FromMilliseconds(100));
         public IWebElement StockAvailabilityElement(string itemId) => driver.UsingDriverImplicitTimeout(() => ProductItemBlock(itemId).FindElement(By.CssSelector("p.stock")), TimeSpan.FromMilliseconds(2000));
-        public IWebElement PriceValueElement(string itemId) => driver.UsingDriverImplicitTimeout(() => ProductItemBlock(itemId).FindElement(By.CssSelector("span.price")), TimeSpan.FromMilliseconds(100));
+        public IWebElement PriceValueElement(string itemId) => driver.UsingDriverImplicitTimeout(() => ProductItemBlock(itemId).FindElement(By.CssSelector("ins span.woocommerce-Price-amount")), TimeSpan.FromMilliseconds(100));
 
         private IWebElement ProductHrefElement(string itemId) => driver.UsingDriverImplicitTimeout(() => driver.FindElement(By.XPath($"//a[contains(@href, '{itemId}')]")), TimeSpan.FromMilliseconds(200));
     }
